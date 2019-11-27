95e570278058906331e72ef323dbd2308b47f629
Sander Kools <sanderkools@gmail.com>
2019-11-27 16:15:41 +0100

    adding tests to see if functionality works

---
+++ test/user_test.py
@@ -0,0 +1,41 @@
+import unittest
+from app import create_app, db
+from app.view.models.user import User
+from config import Config
+
+
+class TestConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite://'
+
+
+class UserModelTest(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app(TestConfig)
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
+    def test_password_hashing(self):
+        bro = User(username='bro')
+        bro.set_password('secret')
+        self.assertFalse(bro.check_password('hacker'))
+        self.assertTrue(bro.check_password('secret'))
+
+    def test_bros(self):
+        bro1 = User(username='bro1')
+        bro2 = User(username='bro2')
+        db.session.add(bro1)
+        db.session.add(bro2)
+        db.session.commit()
+        self.assertEqual(bro1.bros.all(), [])
+        self.assertEqual(bro2.bros.all(), [])
+
+
+if __name__ == '__main__':
+    unittest.main()
