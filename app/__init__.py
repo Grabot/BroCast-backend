7875d8981e3a8660dd939abb93282eb70c6a5379
Grabot <SanderKools@gmail.com>
2019-12-01 20:35:17 +0100

    made more changes for the deployment on aws. Should work now

--- app/__init__.py
+++ app/__init__.py
@@ -14,8 +14,7 @@ POSTGRES = {
     'host': 'brocast-1.cg9fwmgrjypi.eu-central-1.rds.amazonaws.com',
     'port': '5432',
 }
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
-%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
@@ -33,3 +32,23 @@ app.register_blueprint(api_bp)
 from app.view import models
 
 
+# db = SQLAlchemy()
+# migrate = Migrate()
+#
+# def create_app(config_class=Config):
+#     app = Flask(__name__)
+#     app.config.from_object(Config)
+#     db.init_app(app)
+#     migrate.init_app(app, db)
+#
+#     # add the routes
+#     from app.view.routes import app_view as home_bp
+#     app.register_blueprint(home_bp)
+#     from app.rest import app_api as api_bp
+#     app.register_blueprint(api_bp)
+#
+#     # add the models
+#     from app.view import models
+#
+#     return app
+#
