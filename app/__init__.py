5c2e72699ccaf1c936016f09d7d3714c2d7ab6f5
Sander Kools <sanderkools@gmail.com>
2019-12-04 16:06:43 +0100

    removed some debug code

--- app/__init__.py
+++ app/__init__.py
@@ -12,14 +12,14 @@ migrate = Migrate()
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(Config)
-    # POSTGRES = {
-    #     'user': os.environ.get('POSTRGES_USER'),
-    #     'pw': os.environ.get('POSTGRES_PASSWORD'),
-    #     'db': os.environ.get('POSTGRES_DATABASE'),
-    #     'host': os.environ.get('POSTGRES_HOST'),
-    #     'port': os.environ.get('POSTGRES_PORT')
-    # }
-    # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+    POSTGRES = {
+        'user': os.environ.get('POSTRGES_USER'),
+        'pw': os.environ.get('POSTGRES_PASSWORD'),
+        'db': os.environ.get('POSTGRES_DATABASE'),
+        'host': os.environ.get('POSTGRES_HOST'),
+        'port': os.environ.get('POSTGRES_PORT')
+    }
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
     db.init_app(app)
     migrate.init_app(app, db)
 
