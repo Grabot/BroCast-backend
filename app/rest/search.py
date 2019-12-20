722a279ee7ef179586d19fe33462bf5f424b4ea8
Sander Kools <sanderkools@gmail.com>
2019-12-20 11:51:45 +0100

    the find bro can also search for bromotion

--- app/rest/search.py
+++ app/rest/search.py
@@ -7,23 +7,26 @@ from sqlalchemy import func
 
 
 class Search(Resource):
-    def get(self, bro):
-        bros = Bro.query.filter(func.lower(Bro.bro_name) == func.lower(bro))
+    def get(self, bro, bromotion):
+        if bromotion == "None":
+            bros = Bro.query.filter(func.lower(Bro.bro_name) == func.lower(bro))
+        else:
+            bros = Bro.query.filter(func.lower(Bro.bro_name) == func.lower(bro)).filter_by(bromotion=bromotion)
         potential_bros = []
         for bro in bros:
             potential_bros.append({'bro_name': bro.bro_name, 'bromotion': bro.bromotion, 'id': bro.id})
         return jsonify({'bros': potential_bros})
 
-    def put(self, bro):
+    def put(self, bro, bromotion):
         pass
 
-    def delete(self, bro):
+    def delete(self, bro, bromotion):
         pass
 
-    def post(self, bro):
+    def post(self, bro, bromotion):
         pass
 
 
 api = Api(app_api)
-api.add_resource(Search, '/api/v1.0/search/<string:bro>', endpoint='search')
+api.add_resource(Search, '/api/v1.0/search/<string:bro>/<string:bromotion>', endpoint='search')
 
