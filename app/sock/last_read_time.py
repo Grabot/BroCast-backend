406ee7ef65115fd717de6db1dabf396ff493fafb
Grabot <SanderKools@gmail.com>
2021-04-10 20:18:32 +0200

    removed some debugging code

--- app/sock/last_read_time.py
+++ app/sock/last_read_time.py
@@ -18,17 +18,14 @@ def update_last_read_time(bro_id, bros_bro_id):
     if bro_associate is None:
         return None
 
-    print("set last read time")
     read_time = datetime.utcnow()
     bro_associate.last_message_read_time_bro = read_time
-    print(bro_associate.last_message_read_time_bro)
     db.session.add(bro_associate)
     db.session.commit()
     return read_time
 
 
 def get_last_read_time_bro(bro_id, bros_bro_id):
-    print("going to read last read time!")
     # Note that the id's are flipped, since we want the read time of the other bro
     bro_associate = BroBros.query.filter_by(bro_id=bros_bro_id, bros_bro_id=bro_id).first()
 
