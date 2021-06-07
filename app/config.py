37f5d5737e894be6db3888ce44cd7c82e0f5c3f6
Grabot <SanderKools@gmail.com>
2021-06-07 22:40:51 +0200

    some improvements

--- app/config.py
+++ app/config.py
@@ -24,16 +24,3 @@ class DevelopmentConfig(Config):
 
     SQLALCHEMY_DATABASE_URI = DB_URL
 
-
-class ManageConfig(Config):
-    POSTGRES_URL = os.environ["POSTGRES_URL_LOCAL"]
-    POSTGRES_PORT = os.environ["POSTGRES_PORT"]
-    POSTGRES_USER = os.environ["POSTGRES_USER"]
-    POSTGRES_PASSWORD = os.environ["POSTGRES_PASSWORD"]
-    POSTGRES_DB = os.environ["POSTGRES_DB"]
-
-    DB_URL = 'postgresql+psycopg2://{user}:{pw}@{url}:{port}/{db}'.format(
-        user=POSTGRES_USER, pw=POSTGRES_PASSWORD, url=POSTGRES_URL, port=POSTGRES_PORT, db=POSTGRES_DB)
-
-    SQLALCHEMY_DATABASE_URI = DB_URL
-
